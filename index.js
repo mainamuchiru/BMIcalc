c9248136dd3b884e8521cece443db5e28929ac42
Ephantus M Muchiru <emuchiru@usiu.ac.ke>
2022-03-15 11:40:51 +0300

    Bmi calc

---
+++ index.js
@@ -0,0 +1,69 @@
+// var http = require('http');
+// var port = 3000;
+
+// var requestHandler = function (request, response ){
+//     console.log(request.url);
+//     response.end('Hello from Node server');
+
+// }
+
+// var server = http.createServer(requestHandler);
+// server.listen(port);
+
+// console.log('Node server started on port 3000');
+
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.set('views' , 'views');
+// app.set('view engine', 'hbs');
+// app.use(express.static('public'));
+
+// app.get('/home', function (request, response){
+//     response.render('home', {name: 'John Doe'})
+
+// });
+
+// app.listen(port);
+// console.log('server is listening on port 3000');
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require ('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended:false});
+
+app.set('views' , 'views');
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+
+
+app.get('/contacts', function (request, response){
+    response.render('contact_us');
+
+
+});
+
+app.post('/process-contacts',urlEncodedParser, function(request, response){
+    var myweight = request.body.weight;
+    var myheight= request.body.height;
+    var BMI = myweight/(myheight*myheight);
+
+    response.end('Your weight is:'+ request.body.weight +' and your height in meters is:'+request.body.height
+                    + ' Then your BMI is:' + BMI );
+                    
+
+                   
+
+
+});
+
+app.listen(port);
+console.log('server is listening on port 3000');
+
+
+
