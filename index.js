c1024c5231d4abd962d442c5e08dab5a3e8ac487
mainamuchiru <mainaephantus@gmail.com>
2022-03-15 22:52:49 +0300

    Test run using old code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// var http = require('http');
-// var port = 3000;
+var http = require('http');
+var port = 3000;
 
-// var requestHandler = function (request, response ){
-//     console.log(request.url);
-//     response.end('Hello from Node server');
+var requestHandler = function (request, response ){
+    console.log(request.url);
+    response.end('Hello from Node server');
 
-// }
+}
 
-// var server = http.createServer(requestHandler);
-// server.listen(port);
+var server = http.createServer(requestHandler);
+server.listen(port);
 
-// console.log('Node server started on port 3000');
+console.log('Node server started on port 3000');
 
 
 // const express = require('express');
@@ -30,40 +30,44 @@
 // console.log('server is listening on port 3000');
 
 
-const express = require('express');
-const app = express();
-const port = process.env.Port || 3000;
-const bodyParser = require ('body-parser');
-const urlEncodedParser = bodyParser.urlencoded({extended:false});
 
-app.set('views' , 'views');
-app.set('view engine', 'hbs');
-app.use(express.static('public'));
+// // Latest Code
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// // const port = process.env.Port || 3000;
+// const bodyParser = require ('body-parser');
+// const urlEncodedParser = bodyParser.urlencoded({extended:false});
+
+// app.set('views' , 'views');
+// app.set('view engine', 'hbs');
+// app.use(express.static('public'));
 
 
 
-app.get('/contacts', function (request, response){
-    response.render('contact_us');
+// app.get('/contacts', function (request, response){
+//     response.render('contact_us');
 
 
-});
+// });
 
-app.post('/process-contacts',urlEncodedParser, function(request, response){
-    var myweight = request.body.weight;
-    var myheight= request.body.height;
-    var BMI = myweight/(myheight*myheight);
+// app.post('/process-contacts',urlEncodedParser, function(request, response){
+//     var myweight = request.body.weight;
+//     var myheight= request.body.height;
+//     var BMI = myweight/(myheight*myheight);
 
-    response.end('Your weight is:'+ request.body.weight +' and your height in meters is:'+request.body.height
-                    + ' Then your BMI is:' + BMI );
+//     response.end('Your weight is:'+ request.body.weight +' and your height in meters is:'+request.body.height
+//                     + ' Then your BMI is:' + BMI );
                     
 
                    
 
 
-});
+// });
 
-app.listen(port);
-console.log('server is listening on port ${port}');
+// app.listen(port);
+// console.log('server is listening on port ${port}');
 
 
 
